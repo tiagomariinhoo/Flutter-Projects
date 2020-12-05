9b21dda7561d4ff6d8ee33a5e2cdadb177f666be
Tiago Marinho <tiago.marinho.797@gmail.com>
2020-12-05 11:09:05 -0300

    finalizando app

--- my_bookmarks/lib/page/bookmarks_page.dart
+++ my_bookmarks/lib/page/bookmarks_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:my_bookmarks/model/bookmark.dart';
 import 'package:my_bookmarks/widget/bookmarks_list_widget.dart';
 
+import 'add_bookmark_page.dart';
+
 class BookmarksPage extends StatelessWidget {
   List<Bookmark> bookmarksList = [
     Bookmark("Flutter", "https://flutter.dev"),
@@ -11,9 +13,22 @@ class BookmarksPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("My bookmarks"),
-        ),
-        body: BookmarksListWidget(bookmarksList));
+      appBar: AppBar(
+        title: Text("My bookmarks"),
+      ),
+      body: BookmarksListWidget(bookmarksList),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: _onFABPressed));
+        },
+        label: Text("Add"),
+      ),
+    );
+  }
+
+  Widget _onFABPressed(BuildContext context) {
+    return AddBookmarkPage();
   }
 }
